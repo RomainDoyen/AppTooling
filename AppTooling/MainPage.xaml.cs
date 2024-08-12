9371539bb0764df7eacb448af59cd8c41a1bdb11
RomainDoyen <romain.doyen97@gmail.com>
2024-08-12 17:36:49 +0400

    Ajout d'un menu

--- AppTooling/MainPage.xaml.cs
+++ AppTooling/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +23,38 @@ namespace AppTooling
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Dictionary<string, Type> pages = new Dictionary<string, Type>()
+        {
+            { "home", typeof(MainPage) },
+            { "clipboard", typeof(ClipboardPage) },
+            { "device", typeof(DevicePage) }
+        };
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private void NavigationViewControl_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var navItemTag = args.InvokedItemContainer.Tag.ToString();
+            NavViewNavigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+        }
+
+        private void NavViewNavigate(string tag, NavigationTransitionInfo transitionInfo)
+        {
+            Type page = null;
+
+            var item = pages.FirstOrDefault(p => p.Key.Equals(tag));
+
+            page = item.Value;
+
+            var previousNavPageType = ContentFrame.CurrentSourcePageType;
+
+            if (page != null && !Equals(previousNavPageType, page))
+            {
+                ContentFrame.Navigate(page, null, transitionInfo);
+            }
         }
     }
 }
